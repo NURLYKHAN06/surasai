16a38738e5d1f7ab3f15316fe4a7b75450679af6
NURLYKHAN <n.salamatoff@gmail.com>
2020-05-22 16:33:03 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,13 +5,7 @@ import router from './router'
 
 // Initialize Firebase
 firebase.initializeApp({
-  apiKey: "AIzaSyD82NHYrdg9ReTTncyzEM8GMe88MgDwLIU",
-  authDomain: "surasai.firebaseapp.com",
-  databaseURL: "https://surasai.firebaseio.com",
-  projectId: "surasai",
-  storageBucket: "",
-  messagingSenderId: "69437968197",
-  appId: "1:69437968197:web:5ec99fc8a4c818bf"
+// config date
 });
 
 Vue.config.productionTip = false
